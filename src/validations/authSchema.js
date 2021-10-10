de7f9debd7e9d87ddc1ca1a6ace1f87bbce183ab
Matheus <matheusyagami147@gmail.com>
2021-10-09 23:21:53 -0300

    criado o registerSchema no arquivo authSchema.js

--- src/validations/authSchema.js
+++ src/validations/authSchema.js
@@ -5,4 +5,18 @@ const loginSchema = yup.object({
     password: yup.string().required().min(6).max(12).strict()
 })
 
-export default loginSchema
+const registerSchema = yup.object({
+    username: yup.string().required().min(6).max(12).strict(),
+    email: yup.string().required().max(60).strict(),
+    password: yup.string().required().min(6).max(12).strict(),
+    type: yup.string().required().strict(),
+    area: yup.string().when('type', (type, schema) => {
+        if(type !== 'teacher'){
+            return schema;
+        } else{
+            return schema.required();
+        }
+    })
+})
+
+export {loginSchema, registerSchema }
