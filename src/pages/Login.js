afe5d552944ad6131b2a8dce5c266a2c262d80e2
Matheus <matheusyagami147@gmail.com>
2021-10-09 21:31:11 -0300

    criados os states de username e password na página de login. Criada a função handleLogin para captar os dados do login, porém não finalizada

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,25 @@
+import { useState } from 'react'
+
 function Login() {
+    let [username, setUsername] = useState('')
+    let [password, setPassword] = useState('')
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+
+        const data = {username, password}
+        console.log(data)
+    }
+
     return (
         <>
-            <div className="login__container">
+            <div className="login__container" onSubmit={handleLogin}>
                 <form action="" className="login__form">
                     <h2 className="login__form__title">login</h2>
                     <label htmlFor="username" className="login__form__label">Username:</label>
-                    <input type="text" className="login__form__input" />
+                    <input type="text" className="login__form__input" onChange={e => setUsername(e.target.value)} />
                     <label htmlFor="username" className="login__form__label">Password:</label>
-                    <input type="password" className="login__form__input" />
+                    <input type="password" className="login__form__input" onChange={e => setPassword(e.target.value)} />
                     <button className="login__form__button">Login</button>
                 </form>
             </div>
