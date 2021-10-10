3eb914e91cbea244ac8406d62bce9665dc456985
Matheus <matheusyagami147@gmail.com>
2021-10-09 22:39:01 -0300

    adicionados os states na página de register. Adicionada verificação do tipo de usuário, caso teacher, o campo de área é renderizado também

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,15 @@
 import { useState } from 'react'
 
 function Register() {
+    let [username, setUsername] = useState('')
+    let [email, setEmail] = useState('')
+    let [password, setPassword] = useState('')
+    let [type, setType] = useState('')
+    let [area, setArea] = useState('')
 
+    const handleRegister = (e) => {
+        e.preventDefault()
+    }
 
     return (
         <>
@@ -9,18 +17,25 @@ function Register() {
                 <form action="" className="register__form">
                     <h2 className="register__title">Register</h2>
                     <label htmlFor="username" className="register__label">Username:</label>
-                    <input type="text" className="register__input" />
+                    <input type="text" className="register__input" onChange={e => setUsername(e.target.value)}/>
                     <label htmlFor="email" className="register__label">Email:</label>
-                    <input type="email" className="register__input" />
+                    <input type="email" className="register__input" onChange={e => setEmail(e.target.value)} />
                     <label htmlFor="password" className="register__label">Password:</label>
-                    <input type="password" className="register__input" />
+                    <input type="password" className="register__input" onChange={e => setPassword(e.target.value)} />
                     <label htmlFor="type" className="register__label">I am:</label>
-                    <select name="school" onChange={e => setType(e.target.value)}>
+                    <select name="type" onChange={e => setType(e.target.value)}>
                       <option value="student">student</option>
-                      <option value="teacher">student</option>
+                      <option value="teacher">teacher</option>
                     </select>
-                    <label htmlFor="type" className="register__label">Area</label>
-                    <input type="text" className="register__input" />
+                    { type === 'teacher' 
+                         ? (
+                        <>
+                            <label htmlFor="type" className="register__label">Area</label>
+                            <input type="text" className="register__input" onChange={e => setArea(e.target.value)} />
+                        </>    
+                        ) 
+                        : ''
+                    }    
                     <button className="register__button">Register</button>
                 </form>
             </div>
