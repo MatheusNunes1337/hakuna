a2fe10a8b72959a1148a8d4442c69889e53066a0
Matheus <matheusyagami147@gmail.com>
2021-10-12 13:33:59 -0300

    className dos inputs, labels e buttons do formulário de cadastro modificados de register para form tanto no Register.js como no arquivo css

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -33,28 +33,28 @@ function Register() {
         <>
             <div className="register__container" onSubmit={handleRegister}>
                 <form action="" className="register__form">
-                    <h2 className="register__title">Register</h2>
-                    <label htmlFor="username" className="register__label">Username:</label>
-                    <input type="text" className="register__input" onChange={e => setUsername(e.target.value)}/>
-                    <label htmlFor="email" className="register__label">Email:</label>
-                    <input type="email" className="register__input" onChange={e => setEmail(e.target.value)} />
-                    <label htmlFor="password" className="register__label">Password:</label>
-                    <input type="password" className="register__input" onChange={e => setPassword(e.target.value)} />
-                    <label htmlFor="type" className="register__label">I am:</label>
-                    <select name="type" className="register__select" onChange={e => setType(e.target.value)}>
+                    <h2 className="form__title">Register</h2>
+                    <label htmlFor="username" className="form__label">Username:</label>
+                    <input type="text" className="form__input" onChange={e => setUsername(e.target.value)}/>
+                    <label htmlFor="email" className="form__label">Email:</label>
+                    <input type="email" className="form__input" onChange={e => setEmail(e.target.value)} />
+                    <label htmlFor="password" className="form__label">Password:</label>
+                    <input type="password" className="form__input" onChange={e => setPassword(e.target.value)} />
+                    <label htmlFor="type" className="form__label">I am:</label>
+                    <select name="type" className="form__select" onChange={e => setType(e.target.value)}>
                       <option value="student">student</option>
                       <option value="teacher">teacher</option>
                     </select>
                     { type === 'teacher' 
                          ? (
                         <>
-                            <label htmlFor="type" className="register__label">Area</label>
-                            <input type="text" className="register__input" onChange={e => setArea(e.target.value)} />
+                            <label htmlFor="type" className="form__label">Area</label>
+                            <input type="text" className="form__input" onChange={e => setArea(e.target.value)} />
                         </>    
                         ) 
                         : ''
                     }    
-                    <button className="register__btn">Register</button>
+                    <button className="form__btn">Register</button>
                 </form>
                 <a href="/" className="form__link">Already have an account?</a>
             </div>
