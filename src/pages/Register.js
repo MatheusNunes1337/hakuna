3eadfdb818a2744d9f880e7f3523fe332e2c94c5
Matheus <matheusyagami147@gmail.com>
2021-10-12 13:50:42 -0300

    nome dos campos do formulário de cadastro modificados para lingua portuguesa

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -33,30 +33,30 @@ function Register() {
         <>
             <div className="register__container" onSubmit={handleRegister}>
                 <form action="" className="register__form">
-                    <h2 className="form__title">Register</h2>
-                    <label htmlFor="username" className="form__label">Username:</label>
+                    <h2 className="form__title">Cadastro</h2>
+                    <label htmlFor="username" className="form__label">Usuário:</label>
                     <input type="text" className="form__input" onChange={e => setUsername(e.target.value)}/>
                     <label htmlFor="email" className="form__label">Email:</label>
                     <input type="email" className="form__input" onChange={e => setEmail(e.target.value)} />
-                    <label htmlFor="password" className="form__label">Password:</label>
+                    <label htmlFor="password" className="form__label">Senha:</label>
                     <input type="password" className="form__input" onChange={e => setPassword(e.target.value)} />
                     <label htmlFor="type" className="form__label">I am:</label>
                     <select name="type" className="form__select" onChange={e => setType(e.target.value)}>
-                      <option value="student">student</option>
-                      <option value="teacher">teacher</option>
+                      <option value="student">estudante</option>
+                      <option value="teacher">professor</option>
                     </select>
                     { type === 'teacher' 
                          ? (
                         <>
-                            <label htmlFor="type" className="form__label">Area</label>
+                            <label htmlFor="type" className="form__label">Área</label>
                             <input type="text" className="form__input" onChange={e => setArea(e.target.value)} />
                         </>    
                         ) 
                         : ''
                     }    
-                    <button className="form__btn">Register</button>
+                    <button className="form__btn">Cadastrar</button>
                 </form>
-                <a href="/" className="form__link">Already have an account?</a>
+                <a href="/" className="form__link">Já possui uma conta?</a>
             </div>
         </>
     )
