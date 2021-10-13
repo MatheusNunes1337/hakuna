85653a532340d50676bb39e2462d12a80eef210f
Matheus <matheusyagami147@gmail.com>
2021-10-12 21:25:28 -0300

    criado o diretório de routes e o arquivo Routes.js contendo as rotas de login, register, recover pass e redefine pass

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
+import Routes from './routes/Routes'
+
+/*
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
 import RedefinePass from './pages/auth/RedefinePass'
 import RecoverPass from './pages/auth/RecoverPass'
+*/
+
 
 function App() {
   return (
-    <RecoverPass />
+    <Routes />
   );
 }
 
---
+++ src/routes/Routes.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Login from '../pages/auth/Login'
+import Register from '../pages/auth/Register'
+import RecoverPass from '../pages/auth/RecoverPass'
+import RedefinePass from '../pages/auth/RedefinePass'
+
+function Routes() {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/login">
+                    <Login />
+                </Route>
+                <Route path="/register">
+                    <Register />
+                </Route>
+                <Route path="/recover-pass">
+                    <RecoverPass />
+                </Route>
+                <Route path="/redefine-pass">
+                    <RedefinePass />
+                </Route>
+            </Switch>
+        </Router>
+    )
+}
+
+export default Routes
