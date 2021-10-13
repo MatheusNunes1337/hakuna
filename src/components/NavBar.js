630bee3aaf397748c43261823932f6db3b32e116
Matheus <matheusyagami147@gmail.com>
2021-10-13 00:39:40 -0300

    adiciona a tag header ao redor da tag nav do componente de NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,18 +10,18 @@ import Container from './Container'
 function NavBar() {
 
     return (
-      <> 
-        <nav className="navbar">
-            
-                  <Link to="/home" className="navbar__brand">Hakuna</Link>
-                  <FaBars className="navbar__bars"/>
-                  <ul className="navbar__menu">
-                      <li className="navbar__links"><Link to="/home" className="navbar__link"><BiMessageSquareAdd className="navbar__link__icon"/>Grupo</Link></li>
-                      <li className="navbar__links"><Link to="/home" className="navbar__link"><FaCrown className="navbar__link__icon" />Ranking</Link></li>
-                      <button className="navbar__btn"><HiLogout className="navbar__btn__icon" />Sair</button>
-                  </ul>
-                
-        </nav>   
+      <>
+        <header>
+          <nav className="navbar">
+              <Link to="/home" className="navbar__brand">Hakuna</Link>
+              <FaBars className="navbar__bars"/>
+              <ul className="navbar__menu">
+                  <li className="navbar__links"><Link to="/home" className="navbar__link"><BiMessageSquareAdd className="navbar__link__icon"/>Grupo</Link></li>
+                  <li className="navbar__links"><Link to="/home" className="navbar__link"><FaCrown className="navbar__link__icon" />Ranking</Link></li>
+                  <button className="navbar__btn"><HiLogout className="navbar__btn__icon" />Sair</button>
+              </ul> 
+          </nav>
+        </header>   
       </> 
     )
 }
