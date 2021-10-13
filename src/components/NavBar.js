bc8db0c486164c171ac51b231ebda9039addce2e
Matheus <matheusyagami147@gmail.com>
2021-10-12 22:06:10 -0300

    instalado o pacote react-icons. Adicionada o navBar brand no componente de NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import Container from './Container'
 
 function NavBar() {
+
+    function oi() {
+        alert('oi')
+    }
+
     return (
       <>
       <header>
           <Container>
-              <p>oi</p>
+              <nav className="navbar">
+                  <Link to="/home" className="navbar__brand">Hakuna</Link>
+              </nav>
           </Container>
       </header>
       </> 
