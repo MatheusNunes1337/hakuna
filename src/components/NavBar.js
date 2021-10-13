96aa80b966097438c23622727f9a315e4ed7c67d
Matheus <matheusyagami147@gmail.com>
2021-10-12 22:20:12 -0300

    adicionado o navbar__bars, navbar__menu, navbar__links e navbar__links no componente de navBar, porém sem estilização

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import { FaBars, FaCrown } from "react-icons/fa"
+import { BiMessageSquareAdd } from "react-icons/bi"
+import { HiLogout } from "react-icons/hi"
+
+
+
 import Container from './Container'
 
 function NavBar() {
-
-    function oi() {
-        alert('oi')
-    }
 
     return (
       <>
@@ -15,6 +17,12 @@ function NavBar() {
           <Container>
               <nav className="navbar">
                   <Link to="/home" className="navbar__brand">Hakuna</Link>
+                  <FaBars color="#000" className="navbar__bars"/>
+                  <ul className="navbar__menu">
+                      <li className="navbar__links"><Link to="/home" className="navbar__link"><BiMessageSquareAdd />Grupo</Link></li>
+                      <li className="navbar__links"><Link to="/home" className="navbar__link"><FaCrown />Ranking</Link></li>
+                      <button className="navbar__btn"><HiLogout />Sair</button>
+                  </ul>
               </nav>
           </Container>
       </header>
