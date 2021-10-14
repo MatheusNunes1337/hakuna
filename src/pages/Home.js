26ab8a7a103680c3de2221c72be7293e875e8fc3
Matheus <matheusyagami147@gmail.com>
2021-10-13 21:29:52 -0300

    além do Aside foi criado a div .content que vai conter o conteúdo principal da página de home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
 import NavBar from '../components/NavBar'
+import Container from '../components/Container'
+import Aside from '../components/Aside'
 
 function Home() {
     return (
       <>
+      <div className="wrapper">
         <NavBar />
+        <main>
+          <Container>
+            <Aside />
+            <div className="content">
+                <p>content</p>
+            </div>
+          </Container >  
+        </main>
+       </div>    
       </>  
     )
 }
