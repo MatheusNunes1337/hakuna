af7773f28ac23754370a2b8a2b9b2c4fd0879f65
Matheus <matheusyagami147@gmail.com>
2021-10-17 20:58:54 -0300

    removidos os card__topics do componente de card e adicionada a div card__details com os card_detail que mostram se o grupo é privado ou publico e quantos membros há no grupo

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,23 @@
 import React from 'react'
 
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { FaUserAlt } from "react-icons/fa"
+import { RiLock2Fill } from "react-icons/ri";
+import { MdPublic } from "react-icons/md";
+
+
 
 function Card(props) {
 
-    function oi(e) {
-        e.preventDefault();
-        alert('oie')
-    }
-
     return (
-      <Link className="card" to="/home" onClick={oi}>
+      <Link className="card" to="/home">
           <img src="https://www.freeiconspng.com/uploads/whatsapp-icon-png-4.png" alt="card-image" className="card__image" />
           <div className="card__content">
               <h2 className="card__title">Grupo de astronomia</h2>
-              <span className="card__topics">Galáxias</span>
-              <span className="card__topics">Planetas</span>
-              <span className="card__topics">Supernovas</span>
+              <div className="card__details">
+                <span className="card__detail"><RiLock2Fill className="card__detail__icon" />Privado</span>
+                <span className="card__detail"><FaUserAlt className="card__detail__icon" />1/99</span>
+              </div>
             </div>
       </Link> 
     )
