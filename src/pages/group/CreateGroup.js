14524544dcbc59a5184972447c10909561ccc014
Matheus <matheusyagami147@gmail.com>
2021-10-18 22:09:42 -0300

    campos do formulário de criação de grupo criados, porém não estilizados

--- src/pages/group/CreateGroup.js
+++ src/pages/group/CreateGroup.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import NavBar from '../../components/NavBar'
 import Container from '../../components/Container'
 import Aside from '../../components/Aside'
 
 function CreateGroup() {
+    let [name, setName] = useState('')
+    let [description, setDescription] = useState('')
+    let [discipline, setDiscipline] = useState('')
+    let [topics, setTopics] = useState('')
+    let [is_public, setType] = useState(true)
+    let [password, setPassword] = useState('')
+
+     const handleGroup = async (e) => {
+        e.preventDefault()
+
+        console.log('formulário de criação de grupo submetido')
+    }
+
     return (
       <>
         <NavBar />
@@ -13,7 +26,30 @@ function CreateGroup() {
             <Aside />
             <div className="content">
                 <h2 className="content__title">Novo grupo</h2>
-                
+                <form action="" className="group__form" onSubmit={handleGroup}>
+                    <label htmlFor="name" className="form__label">Nome:</label>
+                    <input type="text" className="form__input" onChange={e => setName(e.target.value)} />
+                    <label htmlFor="description" className="form__label">Descrição:</label>
+                    <textarea name="description" className="form__textarea" id="group__description" cols="30" rows="10" onChange={e => setDescription(e.target.value)}></textarea>
+                    <label htmlFor="discipline" className="form__label">Disciplina:</label>
+                    <input type="text" className="form__input" onChange={e => setDiscipline(e.target.value)} />
+                    <label htmlFor="topics" className="form__label">Tópicos (máx: 5, separados entre vírgulas):</label>
+                    <input type="text" className="form__input" onChange={e => setTopics(e.target.value)} />
+                    <select name="type" className="form__select" onChange={e => setType(e.target.value)}>
+                      <option value="true">público</option>
+                      <option value="false">privado</option>
+                    </select>
+                    { !is_public 
+                         ? (
+                        <>
+                            <label htmlFor="password" className="form__label">Senha:</label>
+                            <input type="password" className="form__input" onChange={e => setPassword(e.target.value)} />
+                        </>    
+                        ) 
+                        : ''
+                    }
+                    <button className="form__btn">Criar</button>
+                </form>
             </div>
           </Container >  
         </main>  
