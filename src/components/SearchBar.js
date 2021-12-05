9b2a2042053fae49612627dfc44a096c0cf30562
Matheus Nunes <matheusyagami147@gmail.com>
2021-12-05 15:00:36 -0300

    implementada a função search no componente de SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useHistory } from 'react-router-dom'
 import { BsSearch } from "react-icons/bs"
 
 function SearchBar() {
+  const [filter, setFilter] = useState('')
+  const history = useHistory()
 
-    return (
-      <>
-        <div className="searchbar">
-            <input type="text" className="searchbar__input" placeholder="Busque um grupo por disciplina ou nome"/>
-            <button className="searchbar__btn"><BsSearch className="searchbar__icon"/></button>
-        </div>  
-      </> 
-    )
+  const search = (e) => {
+    e.preventDefault()
+    history.push(`/search?query=${filter}`)
+  }
+
+  return (
+    <>
+      <div className="searchbar">
+          <input type="text" className="searchbar__input" onChange={e => setFilter(e.target.value)} placeholder="Busque um grupo por disciplina ou nome"/>
+          <button className="searchbar__btn" onClick={search}><BsSearch className="searchbar__icon"/></button>
+      </div>  
+    </> 
+  )
 }
 
 export default SearchBar
