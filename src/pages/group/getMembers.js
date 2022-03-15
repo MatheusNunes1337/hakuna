0031f8a8d8f2c96a87126f3890fc5f7fcbfed49d
Matheus Nunes <matheus.silva@compasso.com.br>
2022-03-15 10:10:13 -0300

    feat: develop initial members page

--- src/pages/group/getMembers.js
+++ src/pages/group/getMembers.js
@@ -7,9 +7,9 @@ import Container from '../../components/Container'
 import Aside from '../../components/Aside'
 import SearchBar from '../../components/SearchBar'
 
-import { FaSearch, FaArrowDown } from "react-icons/fa";
+import { FaSearch, FaArrowDown, FaArrowUp } from "react-icons/fa";
+import { BsChevronRight, BsChevronLeft } from "react-icons/bs";
 import { AiFillDelete } from "react-icons/ai";
-
 
 export default function Membros() {
     const { id } = useParams();
@@ -17,6 +17,16 @@ export default function Membros() {
     const userId = localStorage.getItem('userId')
     const headers = { Authorization: `Bearer ${token}` }
     const history = useHistory()
+
+    const [paginationIndex, setPaginationIndex] = useState(1)
+
+    const nextPage = () => {
+        setPaginationIndex(paginationIndex + 1)
+    }
+
+    const previousPage = () => {
+        setPaginationIndex(paginationIndex - 1)
+    }
 
     return (
         <>
@@ -45,12 +55,40 @@ export default function Membros() {
                                     <img src='https://th.bing.com/th/id/OIP.s4XSrU8mt2ats3XCD7pOfgHaF7?pid=ImgDet&w=3000&h=2400&rs=1' className='member__img'/>
                                     <span className='member__name'>Matheus1337</span>
                                     <div className='member__action__btns'>
-                                        <button className='ranking__user__points'><FaSearch /></button>
-                                        <button className='ranking__user__points'><FaArrowDown /></button>
-                                        <button className='ranking__user__points'><AiFillDelete /></button>
+                                        <button className='member__action__btn'><FaSearch /></button>
+                                        <button className='member__action__btn'><FaArrowDown /></button>
+                                        <button className='member__action__btn'><AiFillDelete /></button>
                                     </div>
                                 </div>
                             </div>
+                            <div className='others__container'>
+                                <h3 className="content__subtitle">Outros membros</h3>
+                                <div className='others__item'>
+                                    <span className='member__index'>#1</span>
+                                    <img src='https://th.bing.com/th/id/OIP.s4XSrU8mt2ats3XCD7pOfgHaF7?pid=ImgDet&w=3000&h=2400&rs=1' className='member__img'/>
+                                    <span className='member__name'>Matheus1337</span>
+                                    <div className='member__action__btns'>
+                                        <button className='member__action__btn'><FaSearch /></button>
+                                        <button className='member__action__btn'><FaArrowUp /></button>
+                                        <button className='member__action__btn'><AiFillDelete /></button>
+                                    </div>
+                                </div>
+                                <div className='others__item'>
+                                    <span className='member__index'>#2</span>
+                                    <img src='https://th.bing.com/th/id/OIP.s4XSrU8mt2ats3XCD7pOfgHaF7?pid=ImgDet&w=3000&h=2400&rs=1' className='member__img'/>
+                                    <span className='member__name'>Matheus1337</span>
+                                    <div className='member__action__btns'>
+                                        <button className='member__action__btn'><FaSearch /></button>
+                                        <button className='member__action__btn'><FaArrowUp /></button>
+                                        <button className='member__action__btn'><AiFillDelete /></button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='pagination__wrapper'>
+                            <button disabled={paginationIndex === 1} className='pagination__btn' onClick={previousPage}><BsChevronLeft /></button>
+                            <span className='pagination__index'>{paginationIndex}</span>
+                            <button className='pagination__btn' onClick={nextPage}><BsChevronRight /></button>
                         </div>
                     </div>
                 </Container >  
