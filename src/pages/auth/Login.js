c2923c1ab5c37796858c8fd202804d5ff9fbf140
Matheus Nunes <matheusyagami147@gmail.com>
2022-05-15 23:24:33 -0300

    feat: create login section in login page

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -4,6 +4,8 @@ import api from '../../services/api'
 
 import '../../assets/css/styles.css'
 
+import LoginImage from '../../assets/images/girl_studying.png'
+
 function Login() {
     let [username, setUsername] = useState('')
     let [password, setPassword] = useState('')
@@ -30,11 +32,25 @@ function Login() {
                     <h2 className="form__title">Login</h2>
                     <label htmlFor="username" className="form__label">Usuário:</label>
                     <input type="text" className="form__input" onChange={e => setUsername(e.target.value)} />
-                    <label htmlFor="username" className="form__label">senha:</label>
+                    <label htmlFor="username" className="form__label">Senha:</label>
                     <input type="password" className="form__input" onChange={e => setPassword(e.target.value)} />
                     <button className="form__btn">Login</button>
                     <Link to="/recover-pass" className="form__link">Esqueceu sua senha?</Link>
                 </form>
+                <section className='login__section'>
+                    <div className='login__image__wrapper'>
+                        <img src={LoginImage} alt='girl studying' className='login__image' />
+                    </div>
+                    <form action="" className="form__login" onSubmit={handleLogin}>
+                        <h2 className="form__login__title">Bem-vindo(a) de volta!</h2>
+                        <label htmlFor="username" className="form__label">Nome de usuário</label>
+                        <input type="text" placeholder='Nome de usuário' className="form__input" onChange={e => setUsername(e.target.value)} />
+                        <label htmlFor="username" className="form__label">Senha</label>
+                        <input type="password" placeholder='Senha' className="form__input" onChange={e => setPassword(e.target.value)} />
+                        <button className="form__login__btn">Entrar</button>
+                        <Link to="/recover-pass" className="form__link">Esqueceu sua senha?</Link>
+                    </form>
+                </section>
             </div>
         </>
     )
