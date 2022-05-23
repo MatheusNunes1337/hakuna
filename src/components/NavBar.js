9f3ba9cb8318c4fd15e340a7ef5e74e9b743bf7f
Matheus Nunes <matheus.silva@compasso.com.br>
2022-05-23 11:43:03 -0300

    feat: add contribution points info to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,6 +10,7 @@ import { FaUserAlt } from "react-icons/fa"
 import { HiUserGroup } from "react-icons/hi";
 import { BsFillChatLeftFill, BsFillGearFill } from "react-icons/bs";
 import { IoNotifications } from "react-icons/io5";
+import { AiOutlineLogout } from 'react-icons/ai'
 
 import studentPic from '../assets/images/student.png'
 import crown from '../assets/images/crown.png'
@@ -19,6 +20,7 @@ import chat from '../assets/images/chat.png'
 import settings from '../assets/images/config.png'
 import help from '../assets/images/help.png'
 import addIcon from '../assets/images/add.png'
+import cPoints from '../assets/images/point.png'
 import ErrorModal from './ErrorModal'
 
 function NavBar() {
@@ -26,6 +28,7 @@ function NavBar() {
   let [username, setUsername] = useState('')
   let [type, setType] = useState('')
   let [showErrorModal, setErrorModalStatus] = useState(false)
+  let [contributionPoints, setCPoints] = useState('')
   let [modalMessage, setModalMessage] = useState('')
   const history = useHistory()
 
@@ -37,9 +40,10 @@ function NavBar() {
     const getUser = async () => {
       try {
         const {data} = await api.get(`users/${id}`, {headers})
-        const {username, type} = data
+        const {username, type, contributionPoints} = data
         setType(type)
         setUsername(username)
+        setCPoints(contributionPoints)
       } catch(err) {
         handleErrorModal(err.response.data.name)
       }
@@ -79,9 +83,12 @@ function NavBar() {
               <span className='hakuna__version'>Beta</span>
               <FaBars onClick={toggleMenu} className="navbar__bars"/>
               <ul className="navbar__menu">
-                  <li className="navbar__links"><Link to="/create-group" className="navbar__link"><BiMessageSquareAdd className="navbar__link__icon"/>Grupo</Link></li>
+                  <div className="navbar__cPoints__wrapper">
+                    <img src={cPoints} alt='pontos de contribuição' title='pontos de contribuição'/>
+                    <span className="navbar__cPoints">{contributionPoints}</span>
+                  </div>
                   <li className="navbar__links"><Link to="/ranking" className="navbar__link"><FaCrown className="navbar__link__icon" />Ranking</Link></li>
-                  <button className="navbar__btn"><HiLogout className="navbar__btn__icon" onClick={logout}/>Sair</button>
+                  <button className="navbar__btn"><AiOutlineLogout className="navbar__btn__icon" onClick={logout}/>Logout</button>
               </ul> 
           </nav>
           {!hiddenMenu
@@ -92,6 +99,10 @@ function NavBar() {
                 <img src={studentPic} alt="user_pic" className="hidden__menu__user" />
               </picture>
               <span className="hidden__menu__username">{username}</span>
+              <div className="user__cPoints__wrapper">
+                <img src={cPoints} alt='pontos de contribuição' title='pontos de contribuição'/>
+                <span className="user__cPoints">{contributionPoints}</span>
+              </div>
               <ul className="user__menu">
                 <li className="user__links"><Link to="/create-group" className="user__link"><img src={addIcon} className="user__link__icon"/>Novo grupo</Link></li>
                 <li className="user__links"><Link to="/home" className="user__link"><img src={crown} className="user__link__icon"/>Ranking</Link></li>
@@ -100,7 +111,7 @@ function NavBar() {
                 <li className="user__links"><Link to="/home" className="user__link"><img src={chat} className="user__link__icon"/>Conversas</Link></li>
                 {type == 'teacher'? <li className="sidebar__links"><Link to="/home" className="sidebar__link"><img src={help} className="user__link__icon"/>Solicitações de ajuda</Link></li> : ''}
                 <li className="user__links"><Link to="/user/config" className="user__link"><img src={settings} className="user__link__icon"/>Configurações</Link></li>
-                <button className="user__menu__btn"><HiLogout className="user__menu__btn__icon" onClick={logout}/>Sair</button>
+                <button className="user__menu__btn"><AiOutlineLogout className="user__menu__btn__icon" onClick={logout}/>Logout</button>
               </ul>
             </div>
           </>    
